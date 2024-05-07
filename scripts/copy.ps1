d2da773468c9f6f0cd1cc0b417aded88dcd78073
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2024-05-07 15:33:52 -0300

    update to stellaris version 3.12.*

--- scripts/copy.ps1
+++ scripts/copy.ps1
@@ -1,6 +1,9 @@
 # Obter o caminho do último diretório mod modificado em ../mod/
 $ultimoDiretorioMod = Get-ChildItem "$(Join-Path (Get-Item $PSScriptRoot).Parent.FullName mod)" | Sort-Object -Property LastWriteTime | Select-Object -Last 1
 
+# Apagar o conteúdo da localização padrão dos mods locais do Stellaris
+Remove-Item -Path "$env:USERPROFILE\Documents\Paradox Interactive\Stellaris\mod\$($ultimoDiretorioMod.Name)" -Recurse -Force
+
 # Copiar o conteúdo do último diretório modificado para a localização padrão dos mods locais do Stellaris
 Copy-Item -Path $ultimoDiretorioMod.FullName -Destination "$env:USERPROFILE\Documents\Paradox Interactive\Stellaris\mod" -Recurse
 
