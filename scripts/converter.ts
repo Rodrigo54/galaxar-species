f87dfcf9908737584a7bb6d1eeb62fd346fac2de
Rodrigo Mesquita <rodrigo54mix@gmail.com>
2024-05-30 20:46:16 -0300

    chore: Update scripts to process new portraits and rooms

--- scripts/converter.ts
+++ scripts/converter.ts
@@ -3,29 +3,40 @@ import { existsSync } from 'node:fs';
 import { mkdir, writeFile } from 'node:fs/promises';
 import { dirname, join, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { clear, listar } from './utils';
 
 const __DIRNAME = dirname(fileURLToPath(import.meta.url));
 
 // Especifica o caminho da pasta e o arquivo de saída
-const PASTA_INICIAL = join(__DIRNAME, '../assets/portraits');
-const PASTA_DESTINO = join(__DIRNAME, '../output/portraits');
+export const PASTA_INICIAL = join(__DIRNAME, '../assets');
+export const PASTA_DESTINO = join(__DIRNAME, '../output');
 
-async function batchFile(arquivos: string[]) {
+export async function batchFile(
+  arquivos: string[],
+  options = {
+    format: 'bc3',
+    quality: 'production',
+    noMips: true,
+    zcmp: 5,
+  }
+) {
   const conteudo = arquivos.map((caminho) => {
     const caminhoRelativo = caminho.replace(PASTA_INICIAL, '');
     const caminhoDestino = join(PASTA_DESTINO, caminhoRelativo).replace(
       '.png',
       '.dds'
     );
-    return `"${caminho}" --format bc3 --quality production --no-mips --zcmp 5 --output "${caminhoDestino}"`;
+    return `"${caminho}" --format ${options.format} --quality ${
+      options.quality
+    } ${options.noMips ? '--no-mips' : ''} --zcmp ${
+      options.zcmp
+    } --output "${caminhoDestino}"`;
   });
 
   const batch = resolve(__DIRNAME, '../batch.nvdds');
   await writeFile(batch, conteudo.join('\n'));
 }
 
-async function createFolders(pastas: string[]) {
+export async function createFolders(pastas: string[]) {
   if (!existsSync(PASTA_DESTINO)) {
     await mkdir(PASTA_DESTINO, { recursive: true });
   }
@@ -38,28 +49,12 @@ async function createFolders(pastas: string[]) {
   }
 }
 
-async function converter() {
+export async function converter() {
   const executable = join(
     'C:/Program Files/NVIDIA Corporation/NVIDIA Texture Tools/nvtt_export.exe'
   );
   const batchFile = join(process.cwd(), 'batch.nvdds');
-  const result = await $`${executable} --batch-file="${batchFile}"`.text();
+  const result = await $`${executable} --batch-file="${batchFile}"`;
   // const result = await $`pwsh ./scripts/nvdds.ps1`.quiet().text();
   console.log(result);
 }
-
-const main = async () => {
-  console.log('Limpando pasta de destino...');
-  await clear(PASTA_DESTINO);
-  console.log('Encontrando arquivos...');
-  const { arquivos, pastas } = await listar(PASTA_INICIAL);
-  console.log('Criando pastas...');
-  await createFolders(pastas);
-  console.log('Criando arquivo em lote do nvdds...');
-  await batchFile(arquivos);
-  console.log('Convertendo arquivos...');
-  await converter();
-  console.log('Concluído!');
-};
-
-main().then(() => console.log('Fim'));
